5bd2c9321801d1343c9030d91008f281d4a1023f
Diego Andrés Garcia Álvarez <128091534+DieGogar007@users.noreply.github.com>
2024-04-18 23:22:03 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -165,7 +165,7 @@ resource "null_resource" "run_ansible" {
 
   provisioner "local-exec" {
     
-    command = "sleep 60 && ansible-playbook -i 'inventory' --private-key 'priv_key.ssh' vm.yaml -vvv"
+    command = "sleep 30 && ansible-playbook -i '${local_file.ansible_inventory.filename}' --private-key '${local_sensitive_file.private_key.filename}' ansi.yaml -vvv"
 
   }
 
